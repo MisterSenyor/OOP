763ca6ef6b92c811a4e0a0570091405a956a85af
MisterSenyor <shufi.tolea@gmail.com>
2024-12-06 10:13:33 +0200

    initial

--- bricker/gameobjects/Ball.java
+++ bricker/gameobjects/Ball.java
@@ -1,13 +1,19 @@
 package bricker.gameobjects;
 import danogl.*;
 import danogl.collisions.*;
+import danogl.gui.rendering.Renderable;
+import danogl.util.Vector2;
 
-public class Ball {
-    public Ball() {
-
+public class Ball extends GameObject {
+    public Ball(Vector2 topLeft, Vector2 dimensions, Renderable renderable) {
+        super(topLeft, dimensions, renderable);
     }
-    public void onCollisionEnter(GameObject other, Collision collision) {
 
+    @Override
+    public void onCollisionEnter(GameObject other, Collision collision) {
+        super.onCollisionEnter(other, collision);
+        Vector2 newVel = getVelocity().flipped(collision.getNormal());
+        setVelocity(newVel);
     }
     public int getCollisionCounter() {
         return 0;
--- bricker/main/BrickerGameManager.java
+++ bricker/main/BrickerGameManager.java
@@ -1,7 +1,10 @@
 package bricker.main;
 
+import bricker.gameobjects.Ball;
 import danogl.GameManager;
 import danogl.GameObject;
+import danogl.collisions.Layer;
+import danogl.components.CoordinateSpace;
 import danogl.gui.ImageReader;
 import danogl.gui.SoundReader;
 import danogl.gui.UserInputListener;
@@ -10,6 +13,7 @@ import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
 
 public class BrickerGameManager extends GameManager {
+    static final int WALL_WIDTH = 5;
     public BrickerGameManager(String name, Vector2 pos) {super(name, pos);}
 
     @Override
@@ -17,12 +21,34 @@ public class BrickerGameManager extends GameManager {
                                UserInputListener inputListener, WindowController windowController) {
         super.initializeGame(imageReader, soundReader, inputListener, windowController);
 
-        Renderable ballImage = imageReader.readImage("assets/ball.png", true);
-        GameObject ball = new GameObject(Vector2.ZERO, new Vector2(50, 50), ballImage);
-        ball.setVelocity(Vector2.DOWN.mult(100f));
         Vector2 windowDimensions = windowController.getWindowDimensions();
+        // Create background
+        Renderable bgImage = imageReader.readImage("assets/DARK_BG2_small.jpeg", false);
+        GameObject bg = new GameObject(Vector2.ZERO, windowDimensions, bgImage);
+        bg.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
+        gameObjects().addGameObject(bg, Layer.BACKGROUND);
+
+        // Create ball
+        Renderable ballImage = imageReader.readImage("assets/ball.png", true);
+        GameObject ball = new Ball(Vector2.ZERO, new Vector2(50, 50), ballImage);
+        ball.setVelocity(Vector2.DOWN.mult(100f));
         ball.setCenter(windowDimensions.mult(0.5f));
-        gameObjects().addGameObject(ball);
+        gameObjects().addGameObject(ball, Layer.UI);
+
+        // Create paddle
+        Renderable paddleImage = imageReader.readImage("assets/paddle.png", true);
+        GameObject paddle = new GameObject(Vector2.ZERO, new Vector2(100, 15), paddleImage);
+        paddle.setCenter(new Vector2(windowDimensions.x() / 2, windowDimensions.y() - 30));
+        gameObjects().addGameObject(paddle, Layer.UI);
+
+        // Create walls
+        GameObject wall = new GameObject(Vector2.ZERO, new Vector2(WALL_WIDTH, windowDimensions.y()), null);
+        gameObjects().addGameObject(wall, Layer.BACKGROUND);
+        wall = new GameObject(Vector2.ZERO, new Vector2(windowDimensions.x(), WALL_WIDTH), null);
+        gameObjects().addGameObject(wall, Layer.BACKGROUND);
+        wall = new GameObject(new Vector2(windowDimensions.x() - WALL_WIDTH, 0), new Vector2(WALL_WIDTH, windowDimensions.y()), null);
+        gameObjects().addGameObject(wall, Layer.BACKGROUND);
+
     }
 
 //    public void update(float deltaTime) {}
