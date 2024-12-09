6e7c055d680f31b784b2a18cad6698992a95a26a
Tomer <tomer.zilberman.44@gmail.com>
2024-12-09 20:07:43 +0200

    comments on: - Puck - Brick

--- bricker/gameobjects/Brick.java
+++ bricker/gameobjects/Brick.java
@@ -6,17 +6,22 @@ import danogl.collisions.Collision;
 import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
 
+/**
+ * the brick in a bricker game, the object to destroy.
+ * @author Tomer Zilberman
+ */
 public class Brick extends GameObject {
     private CollisionStrategy collisionStrategy;
     private static int brickCounter = 0;
     /**
-     * Construct a new GameObject instance.
+     * Construct a new Brick instance.
      *
      * @param topLeftCorner Position of the object, in window coordinates (pixels).
      *                      Note that (0,0) is the top-left corner of the window.
      * @param dimensions    Width and height in window coordinates.
      * @param renderable    The renderable representing the object. Can be null, in which case
      *                      the GameObject will not be rendered.
+     * @param collisionStrategy what to do when in collision
      */
     public Brick(Vector2 topLeftCorner, Vector2 dimensions, Renderable renderable, CollisionStrategy collisionStrategy) {
         super(topLeftCorner, dimensions, renderable);
@@ -24,6 +29,13 @@ public class Brick extends GameObject {
         brickCounter++;
     }
 
+    /**
+     * in collision, it activates the collision strategy
+     * @param other The GameObject with which a collision occurred.
+     * @param collision Information regarding this collision.
+     *                  A reasonable elastic behavior can be achieved with:
+     *                  setVelocity(getVelocity().flipped(collision.getNormal()));
+     */
     @Override
     public void onCollisionEnter(GameObject other, Collision collision) {
         super.onCollisionEnter(other, collision);
--- bricker/gameobjects/Puck.java
+++ bricker/gameobjects/Puck.java
@@ -8,29 +8,45 @@ import danogl.util.Vector2;
 
 import java.util.Random;
 
+/**
+ * the special ball that falls from a brick. extends from ball.
+ * @author Tomer Zilbermam
+ * @see Ball the regulat ball
+ */
 public class Puck extends Ball{
 
+    // the size factor from the original ball
     private static final float SIZE_FACTOR = 0.75f;
 
     /**
-     * Construct a new GameObject instance.
+     * Construct a new Puck instance.
      *
-     * @param topLeftCorner  Position of the object, in window coordinates (pixels).
-     *                       Note that (0,0) is the top-left corner of the window.
-     * @param dimensions     Width and height in window coordinates.
-     * @param renderable     The renderable representing the object. Can be null, in which case
-     *                       the GameObject will not be rendered.
-     * @param collisionSound
+     * @param topLeftCorner Position of the object, in window coordinates (pixels).
+     *                      Note that (0,0) is the top-left corner of the window.
+     * @param dimensions    Width and height in window coordinates.
+     * @param renderable    The renderable representing the object. Can be null, in which case
+     *                      the GameObject will not be rendered.
+     * @param collisionSound  The sound the puck makes while colliding with another object
      */
     public Puck(Vector2 topLeftCorner, Vector2 dimensions, Renderable renderable, Sound collisionSound) {
         super(topLeftCorner, dimensions, renderable, collisionSound);
         Random random = new Random();
+        //creates it in a random angle facing up
         double angle = random.nextDouble() * Math.PI;
         float velocityX = (float) Math.cos(angle) * BALL_SPEED;
         float velocityY = (float) Math.sin(angle) * BALL_SPEED;
         this.setVelocity(new Vector2(velocityX, velocityY));
     }
 
+    /**
+     * Construct a new Puck instance.
+     *
+     * @param topLeftCorner Position of the object, in window coordinates (pixels).
+     *                      Note that (0,0) is the top-left corner of the window.
+     * @param renderable    The renderable representing the object. Can be null, in which case
+     *                      the GameObject will not be rendered.
+     * @param collisionSound  The sound the puck makes while colliding with another object
+     */
     public Puck(Vector2 topLeftCorner, Renderable renderable, Sound collisionSound) {
         this(topLeftCorner, DEFAULT_SIZE.mult(SIZE_FACTOR), renderable, collisionSound);
     }
